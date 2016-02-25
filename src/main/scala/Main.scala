adc19b7b49b3ad1fbc65620f95310d3a48badd64
Giuseppe Santoro <giuseppe.santoro@stratifiedmedica.com>
2016-02-25 16:30:35 +0000

    timing execution + group by matched + minor fixes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,53 @@
 import com.github.tototoshi.csv.CSVParser
 import org.apache.spark.{SparkConf, SparkContext}
+import org.joda.time.Duration
+import org.joda.time.format.{PeriodFormatter, PeriodFormatterBuilder, PeriodFormat}
 
 object Main {
   def main(args: Array[String]) {
-    val dataPath = "src/main/resources/chap02/hdfs/linkage"
-    val sparkConf = new SparkConf().setAppName("Hello world")
-    val sc = new SparkContext(sparkConf)
+    time { () =>
+      val dataPath = "src/main/resources/chap02/hdfs/linkage"
+      val sparkConf = new SparkConf().setAppName("Data linkage")
+      val sc = new SparkContext(sparkConf)
 
-    val rawBlocks = sc.textFile(dataPath, 2)
+      val rawBlocks = sc.textFile(dataPath + "/block_1.csv", 10)
+//            .take(10)
 
-    val head = rawBlocks.take(10)
+      val noheader = rawBlocks
+        .filter(isNotHeader)
 
-    val noheader = head
-      .filterNot(isHeader)
+      val parsed = noheader
+        .flatMap(parse)
 
-    noheader
-      .flatMap(parse)
-      .foreach(println)
+      val result = parsed
+        .groupBy(md => md.matched)
+        .mapValues(x => x.size)
+
+      result
+        .foreach(println)
+    }
+  }
+
+  def time(execution: () => Unit): Unit = {
+    val start = System.currentTimeMillis()
+
+    execution()
+
+    val end = System.currentTimeMillis()
+
+    val duration = new Duration(end - start)
+    val formatter = new PeriodFormatterBuilder()
+      .appendDays()
+      .appendSuffix("d")
+      .appendHours()
+      .appendSuffix("h")
+      .appendMinutes()
+      .appendSuffix("m")
+      .appendSeconds()
+      .appendSuffix("s")
+      .toFormatter()
+    val formatted = formatter.print(duration.toPeriod())
+    println(s"Execution time: $formatted")
   }
 
   def parse(line: String): Option[MatchData] = {
@@ -34,7 +65,7 @@ object Main {
 
   def toDouble(s: String): Double = if ("?".equals(s)) Double.NaN else s.toDouble
 
-  def isHeader(line: String): Boolean = {
-    line.contains("id_1")
+  def isNotHeader(line: String): Boolean = {
+    ! line.contains("id_1")
   }
 }
